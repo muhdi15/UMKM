04cfbbb4598af704320f82d9fa811dd5334ec6c8
RizkyAdiwijaya10 <rkyywx@gmail.com>
2025-11-10 15:40:15 +0800

    Testing

--- routes/web.php
+++ routes/web.php
@@ -65,8 +65,6 @@ Route::middleware(['auth', 'role:seller'])->prefix('seller')->group(function () 
 Route::middleware(['auth', 'role:user'])->prefix('user')->group(function () {
 <<<<<<< HEAD
     Route::get('/umkm', [Pembeli::class, 'kategori'])->name('user.umkm');
-=======
-
     Route::get('/home',[Pembeli::class,'userDashboard'])->name('user.dashboard');
     Route::get('/umkm',[Pembeli::class,'kategori'])->name('user.umkm');
     Route::get('/about',[Pembeli::class,'about'])->name('user.about');
